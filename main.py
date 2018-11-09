d49fd049cb891f3066d6dc6c5d0d9f4e2648c441
Santhanagopalan K <sgopalan58@gmail.com>
2018-11-10 02:43:33 +0530

    Completed Backprop

--- main.py
+++ main.py
@@ -7,22 +7,28 @@ class NN():
         self.biases=[np.random.randn(y,1) for y in sizeOfNeuralNetwork[1:]]
         self.alpha=alpha
     def run(self,trainingData):
-        epochs=25
+        m=len(trainingData)
+        epochs=60
         for j in range(epochs):
+            dBiases= [np.zeros(b.shape) for b in self.biases]
+            dWeights = [np.zeros(w.shape) for w in self.weights]
             for x,y in trainingData:
-                dBiases,dWeights=self.full(x,y)
-                self.weights=[weight-alpha*dWeight/m for weight,dWeight in zip(self.weights,dWeights)]
-                self.biases=[bias-alpha*dBias/m for weight,dBias in zip(self.biases,dBiases)]
+                dBiasInter,dWeightInter=self.full(x,y)
+                dBiases = [nb+dnb for nb, dnb in zip(dBiasInter, dBiases)]
+                dWeights = [nw+dnw for nw, dnw in zip(dWeightInter, dWeights)]
+            self.weights=[weight-self.alpha*dWeight/m for weight,dWeight in zip(self.weights,dWeights)]
+            self.biases=[bias-self.alpha*dBias/m for bias,dBias in zip(self.biases,dBiases)]
+            print("Epoch",j,"Value=",self.evaluate(testingData))
     def full(self,x,y):
         dBiases = [np.zeros(b.shape) for b in self.biases]
         dWeights =[np.zeros(w.shape) for w in self.weights]
         Z,Activation=self.forward(x)
         A=Activation[self.size-1]
-        print(Activation[self.size-1].shape)
-        print(self.loss(A,y))
-        dThisBiases,dThisWeights=self.backprop(A,Z,Activation)
+        #print(Activation[self.size-1].shape)
+        #print(self.loss(A,y))
+        dThisBiases,dThisWeights=self.backprop(A,Z,Activation,y)
         dBiases=[currentdBias+dBias for currentdBias,dBias in zip(dBiases,dThisBiases)]
-        dWeights=[currentdWeight+dWeight for currentdWeight,dWeight in zip(dweights,dThisWeights)]
+        dWeights=[currentdWeight+dWeight for currentdWeight,dWeight in zip(dWeights,dThisWeights)]
         return dBiases,dWeights
     def loss(self,A,y):
         cost=y*np.log(A)+(1-y)*np.log(1-A)
@@ -40,16 +46,24 @@ class NN():
         return Z,Activation
     def sigmoid(self,Z):
         return 1/(1+np.exp(-Z))
-    """def backprop(A,Z,Activation,Y):
-        dBiases=[]
-        dWeights=[]
-        dA_prev = - (np.divide(Y,A) - np.divide(1 - Y, 1 - A));
+    def backprop(self,A,Z,Activation,Y):
+        dBiases=[0 for i in range(self.size)]
+        dWeights=[0 for i in range(self.size)]
+        dA=A-Y
         for i in range(1,self.size):
-            dBiases.append(dA)
-            dZ=dA*sigmoidPrime(Z[self.size-1-i])
-            dWeights=
-"""
-
+            dBiases[self.size-1-i]=dA
+            dZ=dA*self.sigmoidPrime(Activation[self.size-i])
+            dWeights[self.size-i-1]=np.dot(dZ,Activation[self.size-1-i].T)
+            dA=np.dot(self.weights[self.size-i-1].T,dZ)
+        return dBiases,dWeights
+    def sigmoidPrime(self,A):
+        return A*(1-A)
+    def evaluate(self,testingData):
+        test_results=[]
+        for x,y in testingData:
+            a,result=self.forward(x)
+            test_results.append((int(np.argmax(result[-1])), int(y)))
+        return sum(int(x == y) for (x, y) in test_results)
 
 
 
@@ -57,6 +71,10 @@ class NN():
 
 
 trainingData,validationData,testingData=prepare.load_data_wrapper()
-mnist=NN([784,30,10],0.1)
+trainingData=list(trainingData)
+validationData=list(validationData)
+testingData=list(testingData)
+mnist=NN([784,30,10],3)
 mnist.run(trainingData)
+
 
--- prepare.py
+++ prepare.py
@@ -21,10 +21,10 @@ def load_data_wrapper():
     training_data = zip(training_inputs, training_results)
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
     validation_results = [vectorized_result(y) for y in va_d[1]]
-    validation_data = zip(validation_inputs, validation_results)
+    validation_data = zip(validation_inputs, va_d[1])
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
     testing_results = [vectorized_result(y) for y in te_d[1]]
-    test_data = zip(test_inputs, testing_results)
+    test_data = zip(test_inputs, te_d[1])
     return (training_data, validation_data, test_data)
 
 def vectorized_result(j):
